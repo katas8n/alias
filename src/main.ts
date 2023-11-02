18e296edeba72e6897fec74977e5537dc311fbc6
katas8n <kirka6061512@icloud.com>
2023-11-02 20:18:11 +0100

    alias-testers

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import './style.css';
+
+const invisible = document.getElementById('invisible');
+const visible = document.getElementById('visible');
+
+interface ICard {
+    image?: string;
+    text: string;
+    isVisible?: boolean;
+}
+
+class Card {
+    constructor({ image, text, isVisible }: ICard) {
+        const card = document.createElement('div');
+
+        const img = document.createElement('img');
+        img.src = '../public/back-part.png';
+        img.className = 'img';
+
+        const textC = document.createElement('p');
+        textC.className = !isVisible ? 'text' : 'text--active';
+        textC.textContent = text;
+
+        card.append(textC);
+        card.append(img);
+
+        card.className = isVisible ? 'card--active' : 'card';
+        // card.textContent = text;
+
+        const flip = e => {
+            const parentCard = e.target.parentElement;
+
+            [...parentCard.children].forEach(el => {
+                console.log(el);
+            });
+
+            img.src = image;
+            textC.className = 'text--active';
+
+            visible?.append(parentCard);
+
+            // card.removeEventListener('click', flip);
+        };
+
+        card.addEventListener('click', flip);
+
+        return card;
+    }
+}
+
+const card = new Card({ text: 'ACE', image: '../public/front-part.png' });
+console.log(card);
+invisible?.append(card);
